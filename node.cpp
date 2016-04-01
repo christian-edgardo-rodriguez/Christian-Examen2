94d8245d4b9c876947479003210cfc65ffdd47f1
christian-edgardo-rodriguez <christian.rodriguez@unitec.edu>
2016-04-01 13:07:57 -0600

    node.cpp completado

--- node.cpp
+++ node.cpp
@@ -8,23 +8,27 @@ Node::Node(){
 
 }
 Node::Node(Person persona):persona(persona){
-
+	next=new Node(persona);
 }
 Node::~Node(){
 
 }
 Person Node::getValue(){
-
+	return persona;
 }
 Node* Node::getNext(){
-
+	return next;
 }
-void Node::setValue(Person persona){
-
+void Node::setValue(Person newPersona){
+	persona=newPersona;
 }
 void Node::setNext(Node* next){
-
+	this->next=next;
 }
 bool Node::hasNext(){
-
+	if (next!=NULL){
+		return true;
+	}else{
+		return false;
+	}
 }
