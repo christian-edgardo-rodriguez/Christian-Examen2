82e5ab80526eb53ac0f73ea7fd72dbbf7b598cf1
christian-edgardo-rodriguez <christian.rodriguez@unitec.edu>
2016-04-01 12:36:12 -0600

    list.cpp y node.cpp creados

---
+++ list.cpp
@@ -0,0 +1,44 @@
+#include "node.hpp"
+#include "person.hpp"
+
+using namespace std;
+
+List::List(){
+
+}
+List::List(Person persona){
+
+}
+List::~List(){
+
+}
+void List::setHead(Person persona){
+
+}
+Person List::getHead(){
+
+}
+void List::insert(int posicion, Person persona){
+
+}
+Person List::at(int posicion){
+
+}
+void List::erase(int posicion){
+
+}
+void List::concat(List* lista){
+
+}
+int List::find(Person persona){
+
+}
+int List::size(){
+
+}
+void List::push_back(Person persona){
+
+}
+Node* List::first(){
+	
+}
---
+++ node.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include "person.hpp"
+
+using namespace std;
+
+Node::Node(){
+
+}
+Node::Node(Person persona){
+
+}
+Node::~Node(){
+
+}
+Person Node::getValue(){
+
+}
+Node* Node::getNext(){
+
+}
+void Node::setValue(Person persona){
+
+}
+void Node::setNext(Node* next){
+
+}
+bool Node::hasNext(){
+
+}
